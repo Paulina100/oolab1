8dae1a0de2f6876b08d2d77c1b11694e02841173
Paulina100 <paulina.jedrychowska@gmail.com>
2021-10-26 22:56:24 +0200

    Zad 2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,77 +1,154 @@
 package agh.ics.oop;
 import static java.lang.System.out;
 
+
 public class World {
-    public static void run(Direction.direction[] tab){
-        for (Direction.direction d : tab){
-            switch (d){
-                case FORWARD:
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case BACKWARD:
-                    out.println("zwierzak idzie do tyłu");
-                    break;
-                case RIGHT:
-                    out.println("zwierzak skręca w prawo");
-                    break;
-                case LEFT:
-                    out.println("zwierzak skręca w lewo");
-                    break;
-            }
+    public static class Vector2d{
+        public final int x;
+        public final int y;
+
+        public Vector2d(int x, int y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public String toString(){
+            return "(" + this.x + ", " + this.y + ")";
+        }
+
+        public boolean precedes(Vector2d other){
+            return this.x <= other.x && this.y <= other.y;
+        }
+
+        public boolean follows(Vector2d other){
+            return this.x >= other.x && this.y >= other.y;
+        }
+
+        public Vector2d upperRight(Vector2d other){
+            int a = Math.max(this.x, other.x);
+            int b = Math.max(this.y, other.y);
+
+            return new Vector2d(a, b);
+        }
+
+        public Vector2d lowerLeft(Vector2d other){
+            int a = Math.min(this.x, other.x);
+            int b = Math.min(this.y, other.y);
+
+            return new Vector2d(a, b);
+        }
+
+        public Vector2d add(Vector2d other){
+            return new Vector2d(this.x + other.x, this.y + other.y);
+        }
+
+        public Vector2d subtract(Vector2d other){
+            return new Vector2d(this.x - other.x, this.y - other.y);
+        }
+
+        public boolean equals(Object other){
+            if (this == other)
+                return true;
+            if (!(other instanceof Vector2d))
+                return false;
+            Vector2d that = (Vector2d) other;
+
+            return this.x == that.x && this.y == that.y;
+        }
+
+        public Vector2d opposite(){
+            return new Vector2d(-this.x, -this.y);
         }
     }
-    /*
-    public static void run(String[] tab) {
 
-        for (String d : tab){
-            switch (d){
-                case "f":
-                    out.println("zwierzak idzie do przodu");
-                    break;
-                case "b":
-                    out.println("zwierzak idzie do tyłu");
-                    break;
-                case "r":
-                    out.println("zwierzak skręca w prawo");
-                    break;
-                case "l":
-                    out.println("zwierzak skręca w lewo");
-                    break;
+    enum MoveDirection{
+        FORWARD,
+        BACKWARD,
+        RIGHT,
+        LEFT
+    }
+
+    public enum MapDirection{
+        NORTH,
+        SOUTH,
+        WEST,
+        EAST;
+
+        public String toString(){
+            return switch (this) {
+                case NORTH -> "Północ";
+                case SOUTH -> "Południe";
+                case WEST -> "Zachód";
+                case EAST -> "Wschód";
+            };
+        }
+
+        public MapDirection next(){
+            return switch (this) {
+                case NORTH -> EAST;
+                case EAST -> SOUTH;
+                case SOUTH -> WEST;
+                case WEST -> NORTH;
+            };
+        }
+
+        public MapDirection previous(){
+            return switch (this) {
+                case EAST -> NORTH;
+                case SOUTH -> EAST;
+                case WEST -> SOUTH;
+                case NORTH -> WEST;
+            };
+        }
+
+        public Vector2d toUnitVector(){
+            return switch (this) {
+                case EAST -> new Vector2d(1, 0);
+                case SOUTH -> new Vector2d(0, -1);
+                case WEST -> new Vector2d(-1, 0);
+                case NORTH -> new Vector2d(0, 1);
+            };
+        }
+    }
+
+    public static void run(Direction.direction[] tab){
+        for (Direction.direction d : tab){
+            switch (d) {
+                case FORWARD -> out.println("zwierzak idzie do przodu");
+                case BACKWARD -> out.println("zwierzak idzie do tyłu");
+                case RIGHT -> out.println("zwierzak skręca w prawo");
+                case LEFT -> out.println("zwierzak skręca w lewo");
             }
         }
 
-        int x = tab.length;
-        for (String i : tab){
-            out.print(i);
-            out.print(", ");
 
-        }
-    }*/
+    }
 
-    public static void main(String[] args) {
-        out.println("system wystartowal");
+    public static Direction.direction[] change(String[] args){
         int l = args.length;
         Direction.direction[] dir = new Direction.direction[l];
 
-
         for (int i = 0; i < l; i++){
-            switch (args[i]){
-                case "f":
-                    dir[i] = Direction.direction.FORWARD;
-                    break;
-                case "b":
-                    dir[i] = Direction.direction.BACKWARD;
-                    break;
-                case "r":
-                    dir[i] = Direction.direction.RIGHT;
-                    break;
-                case "l":
-                    dir[i] = Direction.direction.LEFT;
-                    break;
+            switch (args[i]) {
+                case "f" -> dir[i] = Direction.direction.FORWARD;
+                case "b" -> dir[i] = Direction.direction.BACKWARD;
+                case "r" -> dir[i] = Direction.direction.RIGHT;
+                case "l" -> dir[i] = Direction.direction.LEFT;
             }
         }
+        return dir;
+    }
 
+    public static void main(String[] args) {
+        out.println("system wystartowal");
+
+
+        Direction.direction[] dir = change(args);
         run(dir);
+
+
+        out.println(MapDirection.EAST.toString());
+
 
         out.print("system zakonczyl dzialanie");
     }
---
+++ src/test/java/agh/ics/oop/Vector2dTest.java
@@ -0,0 +1,167 @@
+package agh.ics.oop;
+
+import agh.ics.oop.World.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class Vector2dTest {
+
+    @Test
+    public void equalsTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+
+        assertTrue(v1.equals(v1));
+        assertTrue(v1.equals(v2));
+        assertTrue(v2.equals(v1));
+
+        assertFalse(v1.equals(v3));
+        assertFalse(v2.equals(v3));
+        assertFalse(v2.equals(v4));
+        assertFalse(v1.equals("kot"));
+        assertFalse(v1.equals(10));
+    }
+
+    @Test
+    public void toStringTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+
+        assertEquals(v1.toString(), "(1, 2)");
+        assertEquals(v2.toString(), "(1, 2)");
+        assertEquals(v3.toString(), "(1, -3)");
+        assertEquals(v4.toString(), "(5, 2)");
+    }
+
+    @Test
+    public void precedesTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+
+        assertTrue(v1.precedes(v2));
+        assertTrue(v2.precedes(v1));
+        assertTrue(v1.precedes(v4));
+        assertTrue(v3.precedes(v2));
+        assertTrue(v3.precedes(v4));
+
+        assertFalse(v4.precedes(v1));
+        assertFalse(v4.precedes(v1));
+        assertFalse(v2.precedes(v3));
+    }
+
+    @Test
+    public void nextTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+
+        assertTrue(v1.follows(v2));
+        assertTrue(v2.follows(v1));
+        assertTrue(v4.follows(v1));
+        assertTrue(v2.follows(v3));
+
+        assertFalse(v3.follows(v2));
+        assertFalse(v3.follows(v4));
+        assertFalse(v1.follows(v4));
+    }
+
+    @Test
+    public void upperrightTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+        Vector2d v5 = new Vector2d(-3, -2);
+        Vector2d v6 = new Vector2d(5, 10);
+
+        assertEquals(v1.upperRight(v1), v1);
+        assertEquals(v1.upperRight(v2), new Vector2d(1, 2));
+        assertEquals(v1.upperRight(v3), new Vector2d(1, 2));
+        assertEquals(v3.upperRight(v1), new Vector2d(1, 2));
+        assertEquals(v1.upperRight(v4), new Vector2d(5, 2));
+        assertEquals(v1.upperRight(v5), new Vector2d(1, 2));
+        assertEquals(v1.upperRight(v6), new Vector2d(5, 10));
+    }
+
+    @Test
+    public void lowerLeftTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+        Vector2d v5 = new Vector2d(-3, -2);
+        Vector2d v6 = new Vector2d(5, 10);
+
+        assertEquals(v1.lowerLeft(v1), v1);
+        assertEquals(v1.lowerLeft(v2), new Vector2d(1, 2));
+        assertEquals(v1.lowerLeft(v3), new Vector2d(1, -3));
+        assertEquals(v3.lowerLeft(v1), new Vector2d(1, -3));
+        assertEquals(v1.lowerLeft(v4), new Vector2d(1, 2));
+        assertEquals(v1.lowerLeft(v5), new Vector2d(-3, -2));
+        assertEquals(v1.lowerLeft(v6), new Vector2d(1, 2));
+    }
+
+    @Test
+    public void addTets(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+        Vector2d v5 = new Vector2d(-3, -2);
+        Vector2d v6 = new Vector2d(5, 10);
+
+        assertEquals(v1.add(v1), new Vector2d(2, 4));
+        assertEquals(v1.add(v2), new Vector2d(2, 4));
+        assertEquals(v1.add(v3), new Vector2d(2, -1));
+        assertEquals(v1.add(v4), new Vector2d(6, 4));
+        assertEquals(v1.add(v5), new Vector2d(-2, 0));
+        assertEquals(v1.add(v6), new Vector2d(6, 12));
+    }
+
+    @Test
+    public void subtrackTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(1, 2);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(5, 2);
+        Vector2d v5 = new Vector2d(-3, -2);
+        Vector2d v6 = new Vector2d(5, 10);
+
+        assertEquals(v1.subtract(v1), new Vector2d(0, 0));
+        assertEquals(v1.subtract(v2), new Vector2d(0, 0));
+        assertEquals(v1.subtract(v3), new Vector2d(0, 5));
+        assertEquals(v1.subtract(v4), new Vector2d(-4, 0));
+        assertEquals(v1.subtract(v5), new Vector2d(4, 4));
+        assertEquals(v1.subtract(v6), new Vector2d(-4, -8));
+    }
+
+    @Test
+    public void oppositeTest(){
+        Vector2d v1 = new Vector2d(1, 2);
+        Vector2d v2 = new Vector2d(0, 0);
+        Vector2d v3 = new Vector2d(1, -3);
+        Vector2d v4 = new Vector2d(-5, 2);
+        Vector2d v5 = new Vector2d(-3, -2);
+        Vector2d v6 = new Vector2d(5, 10);
+        Vector2d v7 = new Vector2d(0, 10);
+        Vector2d v8 = new Vector2d(5, 0);
+
+        assertEquals(v1.opposite(), new Vector2d(-1, -2));
+        assertEquals(v2.opposite(), new Vector2d(0, 0));
+        assertEquals(v3.opposite(), new Vector2d(-1, 3));
+        assertEquals(v4.opposite(), new Vector2d(5, -2));
+        assertEquals(v5.opposite(), new Vector2d(3, 2));
+        assertEquals(v6.opposite(), new Vector2d(-5, -10));
+        assertEquals(v7.opposite(), new Vector2d(0, -10));
+        assertEquals(v8.opposite(), new Vector2d(-5, 0));
+    }
+}
