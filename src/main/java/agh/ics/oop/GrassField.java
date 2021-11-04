f16ba33111258158c47e9a656babd33742c77d21
Paulina100 <paulina.jedrychowska@gmail.com>
2021-11-04 17:17:55 +0100

    Zad 5 beginning

---
+++ src/main/java/agh/ics/oop/GrassField.java
@@ -0,0 +1,15 @@
+package agh.ics.oop;
+
+public class GrassField implements IWorldMap {
+    int grassAmount;
+
+    public GrassField(int grassAmount){
+        this.grassAmount = grassAmount;
+    }
+
+    public int getRandomNumber(int min, int max) {
+        return (int) (Math.random() * grassAmount);
+    }
+
+
+}
